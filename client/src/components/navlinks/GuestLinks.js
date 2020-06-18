52cf640a09eb0088f66bd028f13f85e98ce336e8
Phillip <phillipjferreira@gmail.com>
2020-06-18 16:06:53 -0400

    Finish case change

---
+++ client/src/components/navlinks/GuestLinks.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import { NavItem, NavLink, Nav, NavbarToggler, Collapse } from 'shards-react';
+import { NavLink as RouteNavLink } from 'react-router-dom';
+
+const GuestLinks = () => {
+  const [menuOpen, toggleMenuOpen] = useState({
+    open: false,
+  });
+
+  const toggleMenu = () => {
+    toggleMenuOpen({ open: !menuOpen.open });
+  };
+
+  return (
+    <div>
+      <Collapse
+        className='header-navbar d-lg-flex p-0 bg-white border-top'
+        open={menuOpen.open}
+        navbar
+        onClick={toggleMenu}
+      >
+        <Nav navbar className='border-0 flex-column flex-lg-row header-navbar'>
+          <NavItem>
+            <NavLink
+              tag={RouteNavLink}
+              to='/demo-login'
+              className='text-nowrap py-4'
+            >
+              Demo User
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              tag={RouteNavLink}
+              to='/register'
+              className='text-nowrap py-4'
+            >
+              Register
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              tag={RouteNavLink}
+              to='/login'
+              className='text-nowrap py-4'
+            >
+              Login
+            </NavLink>
+          </NavItem>
+        </Nav>
+      </Collapse>
+      <NavbarToggler
+        onClick={toggleMenu}
+        className='border-0 outline-override d-md-inline d-lg-none'
+      />
+    </div>
+  );
+};
+
+export default GuestLinks;
